d4084b37d8e9d67c55c71f2198d0870fb0a1239e
James Antill <james@and.org>
2018-06-29 01:18:03 -0400

    If the container starts failing call it a system error.

--- ci-cmds/i-test-install.sh
+++ ci-cmds/i-test-install.sh
@@ -10,11 +10,14 @@ printf "%38s => " $1 >> $2
 dnf history > /tmp/ohist
 echo "Testing: dnf module install -y $1"
 if dnf module install -y "$1"; then
-  dnf history > /tmp/nhist
-  if [ "x$(cat /tmp/ohist)" != "x$(cat /tmp/nhist)" ]; then
-    echo "   pass" >> $2
+  if dnf history > /tmp/nhist; then
+    if [ "x$(cat /tmp/ohist)" != "x$(cat /tmp/nhist)" ]; then
+      echo "   pass" >> $2
+    else
+      echo "** FAIL: Did nothing fast **" >> $2
+    fi
   else
-    echo "** FAIL: Did nothing fast **" >> $2
+    echo '** FAIL: SYS error **' >> $2
   fi
 else
   echo '** FAIL: DNF error **' >> $2
